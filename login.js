4e27193a3598c0b5330f7868d4fdbc5b58dbbf91
YoshiKitaguchi <ykita0224@gmail.com>
2019-10-03 01:13:32 -0400

    Luke's work

---
+++ login.js
@@ -0,0 +1,46 @@
+// updates the dropdown menus
+$(function(){
+    $("#monthDropdown").on('click', 'li a', function(){
+        $(".btn1:first-child").text($(this).text());
+        $(".btn1:first-child").val($(this).text());
+    });
+});
+$(function(){
+    $("#dayDropdown").on('click', 'li a', function(){
+        $(".btn2:first-child").text($(this).text());
+        $(".btn2:first-child").val($(this).text());
+    });
+});
+$(function(){
+    $("#yearDropdown").on('click', 'li a', function(){
+        $(".btn3:first-child").text($(this).text());
+        $(".btn3:first-child").val($(this).text());
+    });
+});
+
+
+// makes sure the email is good by comparing with the regex
+var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var email = document.getElementById("email");
+function confirmEmail(){
+    if (email.value.match(emailFormat)){
+        console.log("it's ok");
+        email.setCustomValidity('');
+    } else {
+        email.setCustomValidity("Please enter a real email address");
+    }
+}
+email.onchange = confirmEmail;
+
+// confirms the password entered is the same
+var password = document.getElementById("password"); 
+var confirm_password = document.getElementById("repeatPassword");
+function confirmPassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+password.onchange = confirmPassword;
+confirm_password.onkeyup = confirmPassword;
