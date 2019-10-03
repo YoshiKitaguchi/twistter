70b34ed09a7e0f81018d448d34174e01df52e28a
YoshiKitaguchi <yoshi0224@gmail.com>
2019-10-03 02:34:58 -0400

    redirection

--- login.js
+++ login.js
@@ -1,46 +1,21 @@
-// updates the dropdown menus
-$(function(){
-    $("#monthDropdown").on('click', 'li a', function(){
-        $(".btn1:first-child").text($(this).text());
-        $(".btn1:first-child").val($(this).text());
+
+var username;
+var password;
+// function loginbtn_click( id) 
+// {
+//     console.log("clicked!!!");
+//     username = 9;
+//     password = 10;
+//     console.log(username);
+//     console.log(password);
+//     window.location.href = "homepage.html";
+// }
+
+$(document).ready(function() {
+    $("#loginbtn").click(function() {
+        username = document.getElementById("emailUser").value;
+        //console.log(username);
+        password = document.getElementById("password").value;
+        window.location.assign('homepage.html');
     });
 });
-$(function(){
-    $("#dayDropdown").on('click', 'li a', function(){
-        $(".btn2:first-child").text($(this).text());
-        $(".btn2:first-child").val($(this).text());
-    });
-});
-$(function(){
-    $("#yearDropdown").on('click', 'li a', function(){
-        $(".btn3:first-child").text($(this).text());
-        $(".btn3:first-child").val($(this).text());
-    });
-});
-
-
-// makes sure the email is good by comparing with the regex
-var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-var email = document.getElementById("email");
-function confirmEmail(){
-    if (email.value.match(emailFormat)){
-        console.log("it's ok");
-        email.setCustomValidity('');
-    } else {
-        email.setCustomValidity("Please enter a real email address");
-    }
-}
-email.onchange = confirmEmail;
-
-// confirms the password entered is the same
-var password = document.getElementById("password"); 
-var confirm_password = document.getElementById("repeatPassword");
-function confirmPassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
-}
-password.onchange = confirmPassword;
-confirm_password.onkeyup = confirmPassword;
