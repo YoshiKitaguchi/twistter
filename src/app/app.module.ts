4b00e753ef7bb5358c4d50046616493d0bd2691f
Luke Gregurich <lgreguri@purdue.edu>
2019-10-30 20:14:50 -0400

    convert project to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { ContentService } from './shared/services/content.service';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    SignUpComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [/*ContentService*/],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // updates the dropdown menus
+    $(function(){
+      $("#monthDropdown").on('click', 'li a', function(){
+          $(".btn1:first-child").text($(this).text());
+          $(".btn1:first-child").val($(this).text());
+      });
+    });
+    $(function(){
+      $("#dayDropdown").on('click', 'li a', function(){
+          $(".btn2:first-child").text($(this).text());
+          $(".btn2:first-child").val($(this).text());
+      });
+    });
+    $(function(){
+      $("#yearDropdown").on('click', 'li a', function(){
+          $(".btn3:first-child").text($(this).text());
+          $(".btn3:first-child").val($(this).text());
+      });
+    });
+
+    // makes sure the email is good by comparing with the regex
+    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var email = (<HTMLInputElement>document.getElementById("email"));
+    function confirmEmail(){
+      if (email.value.match(emailFormat)){
+          console.log("it's ok");
+          email.setCustomValidity('');
+      } else {
+          email.setCustomValidity("Please enter a real email address");
+      }
+    }
+    email.onchange = confirmEmail;
+
+    // confirms the password entered is the same
+    var password = (<HTMLInputElement>document.getElementById("password")); 
+    var confirm_password = (<HTMLInputElement>document.getElementById("repeatPassword"));
+    function confirmPassword(){
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+      confirm_password.setCustomValidity('');
+    }
+    }
+    password.onchange = confirmPassword;
+    confirm_password.onkeyup = confirmPassword;
+
+    $(document).ready(function() {
+      $("#signup").click(function() {
+          window.location.assign('homepage.html');
+      });
+    });
+
+  }
+
+}
+
